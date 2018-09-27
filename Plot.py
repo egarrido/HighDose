509088378df1b37e854fc297d53df5c3449ae873
egarrido <dr.eric.garrido@gmail.com>
2018-09-27 11:23:47 +0200

    up

---
+++ Plot.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as PLT
+import numpy as np
+
+with open('Output.txt') as f:
+  v = np.loadtxt(f, delimiter=" ", dtype='float', comments="#", skiprows=1, usecols=None)
+v_hist = np.ravel(v)   # 'flatten' v
+fig = PLT.figure()
+ax1 = fig.add_subplot(111)
+
+n, bins, patches = ax1.hist(v_hist, bins=50, normed=1, facecolor='green')
+PLT.show()
