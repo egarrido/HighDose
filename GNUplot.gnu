2f9c0b31ec9a7568be5e5c32950ad54011300a4b
egarrido <dr.eric.garrido@gmail.com>
2018-09-28 09:29:30 +0200

    GNUplot

---
+++ GNUplot.gnu
@@ -0,0 +1,26 @@
+set terminal png size 1200,1200
+set output "Plot.png"
+
+n=100 #number of intervals
+max=0.81 #max value
+min=-0.01 #min value
+width=(max-min)/n #interval width
+#function used to map a value to the intervals
+hist(x,width)=width*floor(x/width)+width/2.0
+set boxwidth width*0.9
+set style fill solid 0.5 # fill style
+
+set multiplot
+set size 1,0.5
+set origin 0,0.5
+set grid
+set xlabel "Position"
+set ylabel "Amplitude"
+set title "Electrons"
+set xrange[min:max]
+#count and plot
+plot "Output_e.txt" u (hist($1,width)):(1.0) smooth freq w boxes lc rgb"red" notitle
+
+set origin 0,0
+set title "Cations"
+plot "Output_c.txt" u (hist($1,width)):(1.0) smooth freq w boxes lc rgb"blue" notitle
---
+++ Vector.hh
@@ -0,0 +1,62 @@
+struct vector_double
+{
+	int dim;
+	double* coef;
+};
+
+typedef struct vector_double* vvector;
+
+vvector vector_create(int n) 
+{
+	vvector r=(vvector)malloc(sizeof(struct vector_double));
+	r->dim=n;
+	r->coef=(double*)malloc(n*sizeof(double));
+	return r;
+}
+
+void vector_free(vvector v) 
+{
+	free(v->coef);
+	free(v);
+}
+
+vvector vector_null(int n) 
+{
+	int i;
+	vvector r=vector_create(n);
+	for(i=0;i<r->dim;i++) 
+	{
+	  r->coef[i]=0.;
+	}
+	return r;
+}
+
+void vector_print(vvector v) 
+{
+	int i;
+	printf("[");
+	for(i=0;i<v->dim;i++) 
+	{
+		printf("%f ",v->coef[i]);
+	}
+	printf("]");
+}
+
+double vector_norm(vvector v) 
+{
+	int i;
+	double x=0.;
+	for(i=0;i<v->dim;i++) 
+	{
+		x+=v->coef[i]*v->coef[i];
+	}
+	return sqrt(x);
+}
+
+// void main() 
+// {
+// 	vvector v=vector_null(3);
+// 	vector_print(v);
+// 	printf(" %f\n",vector_norm(v));
+// 	vector_free(v);
+// }
